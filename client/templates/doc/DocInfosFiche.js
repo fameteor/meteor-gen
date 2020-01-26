8085e1eb19d9d80caebc512a659fc59bac2c2360
francois.artus <francois.artus@orange.com>
2020-01-26 16:03:32 +0100

    File server URL displayed in DOC card

--- client/templates/doc/DocInfosFiche.js
+++ client/templates/doc/DocInfosFiche.js
@@ -7,5 +7,13 @@ Template.DocInfosFiche.helpers({
 	},
 	templateName() {
 		return "DocInfos" + this.codage;
+	},
+	urlServerNormal() {
+		// We remove the first / of tileUrl
+		return Meteor.absoluteUrl()  + this.specif.GEO_REF_tilesUrl.substr(1);
+	},
+	urlServerCropped() {
+		// We remove the first / of tileUrl
+		return Meteor.absoluteUrl()  + this.specif.GEO_REF_tilesUrl.replace("/tiled/","/tiledCropped/").substr(1);
 	}
 });
