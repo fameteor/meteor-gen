8102ed8c0b3e794ce2e1c302116409b335a9270b
francois.artus <francois.artus@orange.com>
2020-03-11 15:49:28 +0100

    First version of fullScreenButton template added

---
+++ client/templates/pers/PersInfosArbreDesc.js
@@ -0,0 +1,1 @@
+import '../../../imports/ui/components/fullScreenButton/fullScreenButton.js';
---
+++ imports/ui/components/fullScreenButton/fullScreenButton.js
@@ -0,0 +1,65 @@
+import './fullScreenButton.html';
+
+/* ---------------------------------------------------------
+Améliorations à faire :
+- comprendre comment ca fonctionne avec boostrap (si interférence)
+- mettre le CSS (ne marche pas)
+- mettre le CSS pour un fond blanc (ne marche pas)
+- passer en paramètre au template le positionnement
+- détecter le fullscreen change et synchronier l'état du bouton avec la réalité (event ne marche pas)
+--------------------------------------------------------- */ 
+
+// ---------------------------------------------------------
+// ON CREATED
+// ---------------------------------------------------------
+Template.fullScreenButton.onCreated (function () {
+	this.isFullScreen = new ReactiveVar(false);
+});
+
+// ---------------------------------------------------------
+// HELPERS
+// ---------------------------------------------------------
+Template.fullScreenButton.helpers({
+	"isFullScreen" : function () {
+		return Template.instance().isFullScreen.get();
+	}
+});
+
+// ---------------------------------------------------------
+// EVENTS
+// ---------------------------------------------------------
+Template.fullScreenButton.events = {
+	'click .fullScreenButton': function (e,tpl) {
+		e.preventDefault();
+		const elem = e.target.parentNode;
+		if (tpl.isFullScreen.get()) {
+			// If fullscreen, exit fullscreen
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+			} else if (document.mozCancelFullScreen) { 	/* Firefox */
+				document.mozCancelFullScreen();
+			} else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+				document.webkitExitFullscreen();
+			} else if (document.msExitFullscreen) { 	/* IE/Edge */
+				document.msExitFullscreen();
+			}
+		}
+		else {
+			// If not fullscreen, go to fullscreen
+			if (elem.requestFullscreen) {
+				elem.requestFullscreen();
+			} else if (elem.mozRequestFullScreen) { 	/* Firefox */
+				elem.mozRequestFullScreen();
+			} else if (elem.webkitRequestFullscreen) { 	/* Chrome, Safari and Opera */
+				elem.webkitRequestFullscreen();
+			} else if (elem.msRequestFullscreen) { 		/* IE/Edge */
+				elem.msRequestFullscreen();
+			}
+		}
+		// Toogle fullscreen state
+		tpl.isFullScreen.set(!tpl.isFullScreen.get());
+	},
+	'mozfullscreenchange, webkitfullscreenchange, fullscreenchange': function (e,tpl) {
+		console.log(document.fullScreenElement);
+	}
+};
